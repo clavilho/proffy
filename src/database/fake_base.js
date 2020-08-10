c8c9cda4c7d6d87e686e3f8f32e4935ee0dfcfe7
clavilho <jv.clavilho@hotmail.com>
2020-08-10 10:59:12 -0300

    Third commit

---
+++ src/database/fake_base.js
@@ -0,0 +1,26 @@
+const proffys = [
+    {
+      name: "Diego Fernandes",
+      avatar: "https://github.com/diego3g.png",
+      whatsapp: "900000000",
+      bio:
+        "Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.00 pessoas já passaram por uma das minhas explosões.",
+      subject: "Química",
+      cost: "20",
+      weekday: [0],
+      time_from: [720],
+      time_to: [1200],
+    },
+    {
+      name: "João Vitor Clavilho",
+      avatar: "https://github.com/clavilho.png",
+      whatsapp: "31988566071",
+      bio:
+        "Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.00 pessoas já passaram por uma das minhas explosões.",
+      subject: "Química",
+      cost: "20",
+      weekday: [1],
+      time_from: [720],
+      time_to: [1200],
+    },
+  ];
---
+++ src/utils/format.js
@@ -0,0 +1,33 @@
+const subjects = [
+  "Artes",
+  "Biologia",
+  "Ciências",
+  "Educação física",
+  "Física",
+  "Geografia",
+  "Historia",
+  "Matemática",
+  "Português",
+  "Química",
+];
+
+const weekdays = [
+  "Domingo",
+  "Segunda",
+  "Terça",
+  "Quarta",
+  "Quinta",
+  "Sexta",
+  "Sbado",
+];
+function getSubject(subjectNumber) {
+  const position = +subjectNumber - 1;
+  return subjects[position];
+}
+
+function convertHoursToMinutes(time) {
+  const [hour, minutes] = time.split(":");
+  return Number (hour * 60 + minutes);
+}
+
+module.exports = [subjects, weekdays, getSubject,convertHoursToMinutes];
